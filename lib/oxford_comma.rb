7ae118850e0b6028d62e1cc53399c638847dd126
Asiful Choudhury <asiful307@gmail.com>
2019-10-14 19:28:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,14 @@ def oxford_comma(array)
  if array.length == 2
     return "#{array[0]} and #{array[1]}"
   elsif 2 < array.length
+<<<<<<< HEAD
     array[-1].insert(0, "and ")
   end
   array.join(", ")    
  end
+=======
+    array[-1].insert("and ")
+  end
+  array.join(", ")    
+ end
+>>>>>>> 7185bbd005006f0e2a31663183b5626ce4ce3a49
